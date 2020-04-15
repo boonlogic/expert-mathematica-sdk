ac9dd76b25f6a6d2fdbd5c9b84441d923ed66897
Elise Courtemanche <elise@boonlogic.com>
2020-04-15 13:56:26 -0500

    Update mma package loading

--- UpdateNano.m
+++ UpdateNano.m
@@ -12,6 +12,7 @@ curr=CreateArchive[FileNameJoin[{Directory[],"NanoREST"}],OverwriteTarget->True]
 app=FileNameJoin[{$UserBaseDirectory,"Applications"}];
 If[MemberQ[FileNames["*",app],FileNameJoin[{app,"NanoREST"}]],DeleteDirectory[FileNameJoin[{app,"NanoREST"}],DeleteContents->True]];
 ExtractArchive[curr,app];
+DeleteFile[curr];
 PrintTemporary["Running Tests...."];
 Print[TestReport@FileNameJoin[{app,"NanoREST", "UnitTests.wlt"}]]
 (*Print["Files updated"];*)
